47d1f1bd4d60a07028f82c1120b0768375d3858a
pedrojfmorais <pedrojfmorais@hotmail.com>
2022-10-18 16:47:06 +0100

    ficha 3 completa

--- Ficha3/src/ex3.java
+++ Ficha3/src/ex3.java
@@ -25,8 +25,57 @@ class Retangulo extends Figura{
         return comprimento * largura;
     }
 }
+class Quadrado extends Figura{
+
+    double lado;
+
+    public Quadrado(double lado) {
+        this.lado = lado;
+    }
+
+    public double area(){
+        return lado * lado;
+    }
+}
 
 public class ex3 {
+
+    public static void main(String[] args) {
+        Comparable<Object> co = new Comparable<Object>() {
+            @Override
+            public int compareTo(Object o) {
+                return 0;
+            }
+        };
+        Comparable<Figura> cf = new Comparable<Figura>() {
+            @Override
+            public int compareTo(Figura o) {
+                return 0;
+            }
+        };
+        Comparable<Retangulo> cr = new Comparable<Retangulo>() {
+            @Override
+            public int compareTo(Retangulo o) {
+                return 0;
+            }
+        };
+
+        Retangulo r = new Retangulo(5, 5);
+        Quadrado q = new Quadrado(5);
+
+        System.out.println("Resultado: " + ex3_b(r, q));
+        System.out.println("Resultado: " + ex3_b(r, r));
+        System.out.println("Resultado: " + ex3_b(r, cr));
+        System.out.println("Resultado: " + ex3_b(r, co));
+        System.out.println("Resultado: " + ex3_b(r, cf));
+
+        System.out.println("Resultado: " + ex3_c(r, q));
+        System.out.println("Resultado: " + ex3_c(r, r));
+        System.out.println("Resultado: " + ex3_c(r, cr));
+        System.out.println("Resultado: " + ex3_c(r, co));
+        System.out.println("Resultado: " + ex3_c(r, cf));
+
+    }
 
     public static int ex3_b(Retangulo rect, Comparable<? super Retangulo> outro){
         return outro.compareTo(rect);
